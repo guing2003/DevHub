22c4d0bbcd09de5a3bdda437dd590f22e8ac11d6
guilh <guilhermedelecrode1710@gmail.com>
2025-12-13 15:45:31 -0300

    Bump version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.delecrode.devhub"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
