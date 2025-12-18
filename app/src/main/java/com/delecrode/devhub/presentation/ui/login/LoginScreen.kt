b2a923821f83a9ff86c9fcc42c32064f1d89cf80
guilh <guilhermedelecrode1710@gmail.com>
2025-12-18 19:00:46 -0300

    Atualizada logo e ajustada a tela de login
    - Adicionado o novo arquivo de imagem `dev_hub_logo.png`.
    - Substituída a imagem `git_logo` pela `dev_hub_logo` na `LoginScreen`.
    - Ajustado o layout da imagem na tela de login, definindo um tamanho fixo, altura para o contêiner e aplicando um `clip` com cantos arredondados.
    - Removido um import não utilizado na `RegisterScreen`.

--- app/src/main/java/com/delecrode/devhub/presentation/ui/login/LoginScreen.kt
+++ app/src/main/java/com/delecrode/devhub/presentation/ui/login/LoginScreen.kt
@@ -7,11 +7,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -29,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -115,11 +120,18 @@ fun LoginScreen(navController: NavController, viewModel: AuthViewModel) {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(220.dp),
+                    contentAlignment = Alignment.Center
+                ) {
                     Image(
-                        painter = painterResource(R.drawable.git_logo),
+                        painter = painterResource(R.drawable.dev_hub_logo),
                         contentDescription = "Logo",
-                        modifier = Modifier.size(100.dp),
+                        modifier = Modifier
+                            .size(220.dp)
+                            .clip(RoundedCornerShape(8.dp))
                     )
                 }
 
